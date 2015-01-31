8cf04b58cafd09c6b570a4182a49ae99abc1da72
Zamith <zamith.28@gmail.com>
2015-01-31 14:39:50 +0000

    Move static file handler to be called after the instance handler
    This way only routes that are not caught by the instance handler will go
    through to the static file handler.

--- spec/frank_handler_spec.cr
+++ spec/frank_handler_spec.cr
@@ -1,4 +1,4 @@
-require "spec_helper"
+require "./spec_helper"
 
 describe "Frank::Handler" do
   it "routes" do
--- spec/route_spec.cr
+++ spec/route_spec.cr
@@ -1,4 +1,4 @@
-require "spec_helper"
+require "./spec_helper"
 
 describe "Route" do
   describe "match" do
