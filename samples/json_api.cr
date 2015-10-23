d0c87f23d19697805f27163e2e2b2e48548cb561
Sdogruyol <dogruyolserdar@gmail.com>
2015-10-23 22:25:46 +0300

    Added json_api sample

--- samples/json_api.cr
+++ samples/json_api.cr
@@ -1,13 +1,9 @@
 require "kemal"
-
-# Set root. If not specified the default content_type is 'text'
-get "/" do
-  "Hello Kemal!"
-end
+require "json"
 
 # You can easily access the environment and set content_type like 'application/json'.
 # Look how easy to build a JSON serving API.
-get "/user.json" do |env|
+get "/" do |env|
   env.response.content_type = "application/json"
   {name: "Serdar", age: 27}.to_json
 end
