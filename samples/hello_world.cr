753c9109890c7a8db4f27acd40bc2697a4c14eca
Sdogruyol <dogruyolserdar@gmail.com>
2015-10-24 22:57:32 +0300

    Removed redundant code from hello_world

--- samples/hello_world.cr
+++ samples/hello_world.cr
@@ -4,10 +4,3 @@ require "kemal"
 get "/" do
   "Hello Kemal!"
 end
-
-# You can easily access the environment and set content_type like 'application/json'.
-# Look how easy to build a JSON serving API.
-get "/user.json" do |env|
-  env.response.content_type = "application/json"
-  {name: "Serdar", age: 27}.to_json
-end
