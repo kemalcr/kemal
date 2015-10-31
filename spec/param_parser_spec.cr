da23d397c6f9878099e4eabbdc3bba590752aef4
Sdogruyol <dogruyolserdar@gmail.com>
2015-10-31 08:53:49 +0200

    Formatted

--- spec/param_parser_spec.cr
+++ spec/param_parser_spec.cr
@@ -3,9 +3,9 @@ require "./spec_helper"
 describe "ParamParser" do
   it "parses query params" do
     route = Route.new "POST", "/" do |env|
-              hasan = env.params["hasan"]
-              "Hello #{hasan}"
-            end
+      hasan = env.params["hasan"]
+      "Hello #{hasan}"
+    end
     request = HTTP::Request.new("POST", "/?hasan=cemal")
     params = Kemal::ParamParser.new(route, request).parse
     params["hasan"].should eq "cemal"
@@ -13,11 +13,11 @@ describe "ParamParser" do
 
   it "parses request body" do
     route = Route.new "POST", "/" do |env|
-              name = env.params["name"]
-              age = env.params["age"]
-              hasan = env.params["hasan"]
-              "Hello #{name} #{hasan} #{age}"
-            end
+      name = env.params["name"]
+      age = env.params["age"]
+      hasan = env.params["hasan"]
+      "Hello #{name} #{hasan} #{age}"
+    end
     request = HTTP::Request.new("POST", "/?hasan=cemal", body: "name=serdar&age=99")
     params = Kemal::ParamParser.new(route, request).parse
     params.should eq({"hasan" => "cemal", "name" => "serdar", "age" => "99"})
