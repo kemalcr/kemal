f658fbe8af9673070b72d92c778806436047a0a4
Oleksii Fedorov <waterlink000@gmail.com>
2015-11-05 11:38:06 +0100

    Parse request body params only if content type is application/x-www-form-urlencoded

--- spec/param_parser_spec.cr
+++ spec/param_parser_spec.cr
@@ -18,8 +18,36 @@ describe "ParamParser" do
       hasan = env.params["hasan"]
       "Hello #{name} #{hasan} #{age}"
     end
-    request = HTTP::Request.new("POST", "/?hasan=cemal", body: "name=serdar&age=99")
+
+    request = HTTP::Request.new(
+      "POST",
+      "/?hasan=cemal",
+      body: "name=serdar&age=99",
+      headers: HTTP::Headers{"Content-Type": "application/x-www-form-urlencoded"},
+    )
+
     params = Kemal::ParamParser.new(route, request).parse
     params.should eq({"hasan" => "cemal", "name" => "serdar", "age" => "99"})
+  end
+
+  context "when content type is incorrect" do
+    it "does not parse request body" do
+      route = Route.new "POST", "/" do |env|
+        name = env.params["name"]
+        age = env.params["age"]
+        hasan = env.params["hasan"]
+        "Hello #{name} #{hasan} #{age}"
+      end
+
+      request = HTTP::Request.new(
+        "POST",
+        "/?hasan=cemal",
+        body: "name=serdar&age=99",
+        headers: HTTP::Headers{"Content-Type": "text/plain"},
+      )
+
+      params = Kemal::ParamParser.new(route, request).parse
+      params.should eq({"hasan" => "cemal"})
+    end
   end
 end
