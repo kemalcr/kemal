47f505ff7603d18e9c9f71bfb4951b57236f07c4
Ahmet Emre Aladağ <aladagemre@gmail.com>
2016-01-05 17:35:07 +0200

    Update view.cr

--- src/kemal/view.cr
+++ src/kemal/view.cr
@@ -1,3 +1,25 @@
+# Template for 403 Forbidden
+def render_403
+  template = <<-HTML
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <style type="text/css">
+        body { text-align:center;font-family:helvetica,arial;font-size:22px;
+          color:#888;margin:20px}
+        #c {margin:0 auto;width:500px;text-align:left}
+        </style>
+      </head>
+      <body>
+        <h2>Forbidden</h2>
+        <h3>Kemal doesn't allow you to see this page.</h3>
+        <img src="/__kemal__/404.png">
+      </body>
+      </html>
+  HTML
+  HTTP::Response.new(403, template)
+end
+
 # Template for 404 Not Found
 def render_404
   template = <<-HTML
