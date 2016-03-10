f70d54ad8eb60bfaf2de2925dd6efdce0adc25e5
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2016-03-11 01:23:34 +0200

    Added check port whether used by another application when starting HTTP Server

--- src/kemal/config.cr
+++ src/kemal/config.cr
@@ -1,3 +1,5 @@
+require "socket"
+
 module Kemal
   class Config
     INSTANCE = Config.new
@@ -44,6 +46,7 @@ module Kemal
       setup_logging
       setup_error_handler
       setup_public_folder
+      check_port
     end
 
     def setup_logging
@@ -53,6 +56,16 @@ module Kemal
                   Kemal::NullLogHandler.new(@env)
                 end
       HANDLERS.insert(0, @logger.not_nil!)
+    end
+
+    def check_port
+      begin
+        TCPSocket.new(@host_binding, @port).close
+        logger.write "#{@port} port is used by another process. Please select a different port.\n"
+        exit
+      rescue
+        true
+      end
     end
 
     private def setup_error_handler
