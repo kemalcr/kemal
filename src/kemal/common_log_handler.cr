ae25d0a7742c4b2fd95d0a0b1acffe59bf388e91
Sdogruyol <dogruyolserdar@gmail.com>
2016-07-05 20:11:03 +0300

    Optimize CommonLogHandler to directly use the handler instead of string interpolation

--- src/kemal/common_log_handler.cr
+++ src/kemal/common_log_handler.cr
@@ -17,9 +17,9 @@ class Kemal::CommonLogHandler < Kemal::BaseLogHandler
   def call(context)
     time = Time.now
     call_next(context)
-    elapsed = Time.now - time
-    elapsed_text = elapsed_text(elapsed)
-    write "#{time} #{context.response.status_code} #{context.request.method} #{context.request.resource} - #{elapsed_text}\n"
+    elapsed_text = elapsed_text(Time.now - time)
+    @handler << time << " " << context.response.status_code << " " << context.request.method << " " << context.request.resource << " " << elapsed_text << "\n"
+    @handler.rewind
     context
   end
 
@@ -34,13 +34,5 @@ class Kemal::CommonLogHandler < Kemal::BaseLogHandler
     return "#{millis.round(2)}ms" if millis >= 1
 
     "#{(millis * 1000).round(2)}µs"
-  end
-
-  def write(message)
-    if @env == "production"
-      @handler.write message.to_slice
-    else
-      @handler.print message
-    end
   end
 end
