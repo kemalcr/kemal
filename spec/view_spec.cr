24e0824a9bf507ff25bea971b3a10d54b1129f58
Sdogruyol <dogruyolserdar@gmail.com>
2016-07-12 00:09:01 +0300

    Better view_specs for layout variables

--- spec/view_spec.cr
+++ spec/view_spec.cr
@@ -35,6 +35,20 @@ describe "Views" do
     client_response.body.should contain("<html>Hello world")
   end
 
+  it "renders layout with variables" do
+    get "/view/:name" do |env|
+      name = env.params.url["name"]
+      var1 = "serdar"
+      var2 = "kemal"
+      render "spec/asset/hello_with_content_for.ecr", "spec/asset/layout_with_yield_and_vars.ecr"
+    end
+    request = HTTP::Request.new("GET", "/view/world")
+    client_response = call_request_on_app(request)
+    client_response.body.should contain("Hello world")
+    client_response.body.should contain("serdar")
+    client_response.body.should contain("kemal")  
+  end
+
   it "renders layout with content_for" do
     get "/view/:name" do |env|
       name = env.params.url["name"]
@@ -42,7 +56,7 @@ describe "Views" do
     end
     request = HTTP::Request.new("GET", "/view/world")
     client_response = call_request_on_app(request)
-    client_response.body.should contain("<html>Hello world")
+    client_response.body.should contain("Hello world")
     client_response.body.should contain("<h1>Hello from otherside</h1>")
   end
 end
