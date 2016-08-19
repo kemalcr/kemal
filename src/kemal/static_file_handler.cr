6280af2e405258cc353e7d6039f3066817398a45
Cris Ward <cris@duodesign.co.uk>
2016-08-19 15:49:29 +0100

    added mime types (#195)
    * added mime types
    
    Added some mime type for images. Very new to crystal so hopefully this is ok.
    Without this they default to octet, which just does a download
    
    * jpg to jpeg

--- src/kemal/static_file_handler.cr
+++ src/kemal/static_file_handler.cr
@@ -9,5 +9,20 @@ module Kemal
       return call_next(context) if context.request.path.not_nil! == "/"
       super
     end
+    
+    def mime_type(path)
+      case File.extname(path)
+      when ".txt"          then "text/plain"
+      when ".htm", ".html" then "text/html"
+      when ".css"          then "text/css"
+      when ".js"           then "application/javascript"
+      when ".png"          then "image/png"
+      when ".jpg", ".jpeg" then "image/jpeg"
+      when ".gif"          then "image/gif"
+      when ".svg"          then "image/svg+xml"
+      else                      "application/octet-stream"
+      end
+    end
+    
   end
 end
