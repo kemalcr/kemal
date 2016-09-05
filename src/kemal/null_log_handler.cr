7c8915bfa4c10f98e065315816a2f689cc78d945
sdogruyol <dogruyolserdar@gmail.com>
2016-09-05 17:55:53 +0300

    Don't forget to call_next on NullLogHandler

--- src/kemal/null_log_handler.cr
+++ src/kemal/null_log_handler.cr
@@ -2,6 +2,7 @@ module Kemal
   # This is here to represent the logger corresponding to Null Object Pattern.
   class NullLogHandler < Kemal::BaseLogHandler
     def call(context)
+      call_next(context)
     end
 
     def write(message)
