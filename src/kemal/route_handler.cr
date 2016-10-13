23107b4a0ac96a5a3aabec56a1beb0a7ce5351bf
Sam Eaton <same@qualtrics.com>
2016-10-13 16:32:06 -0600

    adds only_routes and exclude_routes macros for middlewares

--- src/kemal/route_handler.cr
+++ src/kemal/route_handler.cr
@@ -49,4 +49,38 @@ module Kemal
       @tree.add node, route
     end
   end
+
+  # Extend HTTP::Handler so that certain macros are scoped to HTTP::Handler or
+  # inheriting classes, and not part of the global scope
+  class HTTP::Handler
+    private def to_path_regex(path)
+      path = ("^" + path + "(\/)?$").split("**")
+      path = path.map { |p| p.gsub(/\*/, "[\\w\\d~]*") }
+      Regex.new path.join("[\\w\\d\\/~]*")
+    end
+
+    # Checks if two routes match.
+    # The second path may or may not have a single (*) or double wildcard (**)
+    private def paths_match?(actual_path, dynamic_path)
+      return true if (actual_path === dynamic_path && !dynamic_path.includes? "*")
+      actual_path = actual_path + "/" unless actual_path.ends_with?("/")
+      actual_path =~ to_path_regex dynamic_path
+    end
+
+    # Will only run rest of middleware if the current
+    # route matches one of the given routes
+    macro only_routes(context, routes)
+      return call_next {{context}} unless {{routes}}.any? do |route|
+        paths_match? {{context}}.request.path, route
+      end
+    end
+
+    # Will only run rest of middleware if the current
+    # route does NOT match one of the given routes
+    macro exclude_routes(context, routes)
+      return call_next {{context}} if {{routes}}.any? do |route|
+        paths_match? {{context}}.request.path, route
+      end
+    end
+  end
 end
