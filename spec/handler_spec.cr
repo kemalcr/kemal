edbb12ed8f4cc587a7cd149a21a63a9db7bf04e1
sdogruyol <dogruyolserdar@gmail.com>
2016-10-27 17:34:41 +0300

    Clean specs

--- spec/handler_spec.cr
+++ spec/handler_spec.cr
@@ -10,12 +10,10 @@ end
 describe "Handler" do
   it "adds custom handler before before_*" do
     before_get "/" do |env|
-      puts "Next 1"
       env.response << " is"
     end
 
     before_get "/" do |env|
-      puts "Next 2"
       env.response << " so"
     end
 
@@ -27,11 +25,6 @@ describe "Handler" do
     get "/" do |env|
       " Great"
     end
-
-    Kemal.config.handlers.each do |h|
-      puts h.class
-    end
-
     request = HTTP::Request.new("GET", "/")
     client_response = call_request_on_app(request)
     client_response.status_code.should eq(200)
