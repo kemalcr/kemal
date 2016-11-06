cab2ae4eb03702450f137836350d16a3cbf0ff95
sdogruyol <dogruyolserdar@gmail.com>
2016-11-06 21:30:59 +0300

    Add only_routes and exclude routes to HTTP::Handler

--- spec/route_spec.cr
+++ spec/route_spec.cr
@@ -1,5 +1,27 @@
 require "./spec_helper"
 
+class OnlyHandler < HTTP::Handler
+  def call(env)
+    only_routes env, ["/only"]
+    env.response.print "Only"
+    call_next env
+  end
+
+  def write(message)
+  end
+end
+
+class ExcludeHandler < HTTP::Handler
+  def call(env)
+    exclude_routes env, ["/exclude"]
+    env.response.print "Exclude"
+    call_next env
+  end
+
+  def write(message)
+  end
+end
+
 describe "Route" do
   describe "match?" do
     it "matches the correct route" do
@@ -20,6 +42,26 @@ describe "Route" do
           "Route 1"
         end
       end
+    end
+
+    it "runs specified only_routes in middleware" do
+      get "/" do
+        "Not"
+      end
+      add_handler OnlyHandler.new
+      request = HTTP::Request.new("GET", "/only")
+      client_response = call_request_on_app(request)
+      client_response.body.should eq "Only"
+    end
+
+    it "doesn't run specified exclude_routes in middleware" do
+      get "/exclude" do
+        "Not"
+      end
+      add_handler ExcludeHandler.new
+      request = HTTP::Request.new("GET", "/")
+      client_response = call_request_on_app(request)
+      client_response.body.should eq "Exclude"
     end
   end
 end
