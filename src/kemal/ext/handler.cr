e798605a333d5c97b8d97686dd4abad6474fb06f
sdogruyol <dogruyolserdar@gmail.com>
2016-11-07 18:57:42 +0300

    Middleware routing take 2 (WIP)

---
+++ src/kemal/ext/handler.cr
@@ -0,0 +1,49 @@
+# Extend HTTP::Handler so that certain macros are scoped to HTTP::Handler or
+# inheriting classes, and not part of the global scope
+class HTTP::Handler
+  @@only_routes_tree = Radix::Tree(String).new
+  @@exclude_routes_tree = Radix::Tree(String).new
+
+  private def radix_path(method : String, path)
+    "/#{method.downcase}#{path}"
+  end
+end
+
+class Kemal::Handler < HTTP::Handler
+  macro only(paths, method = "GET")
+    class_name = {{@type.name}}
+    {{paths}}.each do |path|
+      @@only_routes_tree.add "#{class_name}/#{{{method}}.downcase}#{path}", "/#{{{method}}.downcase}#{path}"
+    end
+  end
+
+  macro exclude(paths, method = "GET")
+    class_name = {{@type.name}}
+    {{paths}}.each do |path|
+      @@exclude_routes_tree.add "#{class_name}/#{{{method}}.downcase}#{path}", "/#{{{method}}.downcase}#{path}"
+    end
+  end
+
+  def call(env)
+    if @@only_routes_tree || @@exclude_routes_tree
+      only_found = false
+      exclude_found = false
+      if @@only_routes_tree
+        only_found = @@only_routes_tree.find(radix_path(env.request.method, env.request.path)).found?
+      end
+      if @@exclude_routes_tree
+        exclude_found = @@exclude_routes_tree.find(radix_path(env.request.method, env.request.path)).found?
+      end
+      if only_found && !exclude_found
+        return false
+      else
+        return true
+      end
+    end
+    return false
+  end
+
+  private def radix_path(method : String, path)
+    "#{self.class}/#{method.downcase}#{path}"
+  end
+end
