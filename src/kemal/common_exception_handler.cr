731c3ae9926a0eb3b9a6fa01f0186b53979bcdcc
Jeremy Woertink <jeremywoertink@gmail.com>
2016-11-22 22:04:20 -0800

    working out error with logging

--- src/kemal/common_exception_handler.cr
+++ src/kemal/common_exception_handler.cr
@@ -21,6 +21,7 @@ module Kemal
     def call_exception_with_status_code(context, exception, status_code)
       if Kemal.config.error_handlers.has_key?(status_code)
         context.response.content_type = "text/html" unless context.response.headers.has_key?("Content-Type")
+        context.response.status_code = status_code
         context.response.print Kemal.config.error_handlers[status_code].call(context, exception)
         context
       end
