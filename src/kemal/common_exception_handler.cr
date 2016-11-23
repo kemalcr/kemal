2248475573ca3928607df7318f34af663e3c56a2
sdogruyol <dogruyolserdar@gmail.com>
2016-11-23 20:36:56 +0300

    Properly add status_code on exceptions. Fixes #255

--- src/kemal/common_exception_handler.cr
+++ src/kemal/common_exception_handler.cr
@@ -22,6 +22,7 @@ module Kemal
       if Kemal.config.error_handlers.has_key?(status_code)
         context.response.content_type = "text/html" unless context.response.headers.has_key?("Content-Type")
         context.response.print Kemal.config.error_handlers[status_code].call(context, exception)
+        context.response.status_code = status_code
         context
       end
     end
