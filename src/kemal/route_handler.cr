b47d3707ba62168603ed742015e9810e1b7716ad
Cris Ward <cris@duodesign.co.uk>
2017-02-04 11:44:18 +0000

    Added multipart parser to params object, added params.files

--- src/kemal/route_handler.cr
+++ src/kemal/route_handler.cr
@@ -34,11 +34,18 @@ module Kemal
       raise Kemal::Exceptions::RouteNotFound.new(context) unless context.route_defined?
       route = context.route_lookup.payload.as(Route)
       content = route.handler.call(context)
+      remove_tmpfiles(context)
       if Kemal.config.error_handlers.has_key?(context.response.status_code)
         raise Kemal::Exceptions::CustomException.new(context)
       end
       context.response.print(content)
       context
+    end
+
+    private def remove_tmpfiles(context)
+      context.params.files.each do |field, file|
+        ::File.delete(file.tmpfile) if ::File.exists?(file.tmpfile)
+      end
     end
 
     private def radix_path(method : String, path)
