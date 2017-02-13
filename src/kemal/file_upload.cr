c6ff2c5d2e804274806f538df08cf5a45708d838
Sdogruyol <dogruyolserdar@gmail.com>
2017-02-13 22:57:58 +0300

    Move file_upload to namespace

--- src/kemal/file_upload.cr
+++ src/kemal/file_upload.cr
@@ -1,12 +1,14 @@
 # :nodoc:
-struct FileUpload
-  getter tmpfile : Tempfile
-  getter tmpfile_path : String
-  getter filename : String
-  getter meta : HTTP::FormData::FileMetadata
-  getter headers : HTTP::Headers
+module Kemal
+  struct FileUpload
+    getter tmpfile : Tempfile
+    getter tmpfile_path : String
+    getter filename : String
+    getter meta : HTTP::FormData::FileMetadata
+    getter headers : HTTP::Headers
 
-  def initialize(@tmpfile, @tmpfile_path, @meta, @headers)
-    @filename = @meta.filename.not_nil!
+    def initialize(@tmpfile, @tmpfile_path, @meta, @headers)
+      @filename = @meta.filename.not_nil!
+    end
   end
 end
