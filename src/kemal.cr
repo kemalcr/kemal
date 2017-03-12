kemalcr/kemal

commit 1afb4cbfa8218ab8616c59a041dcc85fa72834a8

Use log instead

src/kemal.cr
@@ -30,7 +30,7 @@ module Kemal
     # Test environment doesn't need to have signal trap, built-in images, and logging.
     unless config.env == "test"
       Signal::INT.trap {
-        config.logger.write "Kemal is going to take a rest!\n"
+        log "Kemal is going to take a rest!\n"
         config.server.close
         exit
       }
@@ -46,7 +46,7 @@ module Kemal
         end
       end
 
-      config.logger.write "[#{config.env}] Kemal is ready to lead at #{config.scheme}://#{config.host_binding}:#{config.port}\n"
+      log "[#{config.env}] Kemal is ready to lead at #{config.scheme}://#{config.host_binding}:#{config.port}\n"
       config.server.listen
     end
   end

src/kemal/common_exception_handler.cr
@@ -11,7 +11,7 @@ module Kemal
       rescue ex : Kemal::Exceptions::CustomException
         call_exception_with_status_code(context, ex, context.response.status_code)
       rescue ex : Exception
-        Kemal.config.logger.write("Exception: #{ex.inspect_with_backtrace}\n")
+        log("Exception: #{ex.inspect_with_backtrace}\n")
         return call_exception_with_status_code(context, ex, 500) if Kemal.config.error_handlers.has_key?(500)
         verbosity = Kemal.config.env == "production" ? false : true
         return render_500(context, ex.inspect_with_backtrace, verbosity)

commit 727727f2d011ef1688c5b4d2ea98b5b849d3f8c4

Fix regression introduced in 1afb4cbfa8218ab8616c59a041dcc85fa72834a8

src/kemal.cr
@@ -10,7 +10,7 @@ module Kemal
   # Overload of self.run with the default startup logging
   def self.run(port = nil)
     self.run port do
-      log "[#{config.env}] Kemal is ready to lead at #{config.scheme}://#{config.host_binding}:#{config.port}\n"
+      log "[#{config.env}] Kemal is ready to lead at #{config.scheme}://#{config.host_binding}:#{config.port}"
     end
   end
 
@@ -42,7 +42,7 @@ module Kemal
     # Test environment doesn't need to have signal trap, built-in images, and logging.
     unless config.env == "test"
       Signal::INT.trap {
-        log "Kemal is going to take a rest!\n" if config.shutdown_message
+        log "Kemal is going to take a rest!" if config.shutdown_message
         Kemal.stop
         exit
       }

src/kemal/common_exception_handler.cr
@@ -12,7 +12,7 @@ module Kemal
       rescue ex : Kemal::Exceptions::CustomException
         call_exception_with_status_code(context, ex, context.response.status_code)
       rescue ex : Exception
-        log("Exception: #{ex.inspect_with_backtrace}\n")
+        log("Exception: #{ex.inspect_with_backtrace}")
         return call_exception_with_status_code(context, ex, 500) if Kemal.config.error_handlers.has_key?(500)
         verbosity = Kemal.config.env == "production" ? false : true
         return render_500(context, ex.inspect_with_backtrace, verbosity)