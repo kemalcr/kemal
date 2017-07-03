4f384bc26b10e734f74cd296cd943d8064eaafa6
Vitaly Tarasenko <vetal.tarasenko@gmail.com>
2017-07-03 19:55:13 +0300

    move DSL methods to DSL module. Allow to include Kemal::DSL to specific controller classes

--- src/kemal/dsl.cr
+++ src/kemal/dsl.cr
@@ -4,32 +4,41 @@
 # - WebSocket(ws)
 # - before_*
 # - error
-HTTP_METHODS   = %w(get post put patch delete options)
-FILTER_METHODS = %w(get post put patch delete options all)
 
-{% for method in HTTP_METHODS %}
-  def {{method.id}}(path, &block : HTTP::Server::Context -> _)
-    raise Kemal::Exceptions::InvalidPathStartException.new({{method}}, path) unless Kemal::Utils.path_starts_with_slash?(path)
-    Kemal::RouteHandler::INSTANCE.add_route({{method}}.upcase, path, &block)
-  end
-{% end %}
+module Kemal
+  module DSL
+    HTTP_METHODS   = %w(get post put patch delete options)
+    FILTER_METHODS = %w(get post put patch delete options all)
 
-def ws(path, &block : HTTP::WebSocket, HTTP::Server::Context -> Void)
-  raise Kemal::Exceptions::InvalidPathStartException.new("ws", path) unless Kemal::Utils.path_starts_with_slash?(path)
-  Kemal::WebSocketHandler.new path, &block
-end
+    {% for method in HTTP_METHODS %}
+      def {{method.id}}(path, &block : HTTP::Server::Context -> _)
+        raise Kemal::Exceptions::InvalidPathStartException.new({{method}}, path) unless Kemal::Utils.path_starts_with_slash?(path)
+        Kemal::RouteHandler::INSTANCE.add_route({{method}}.upcase, path, &block)
+      end
+    {% end %}
 
-def error(status_code, &block : HTTP::Server::Context, Exception -> _)
-  Kemal.config.add_error_handler status_code, &block
-end
-
-# All the helper methods available are:
-#  - before_all, before_get, before_post, before_put, before_patch, before_delete, before_options
-#  - after_all, after_get, after_post, after_put, after_patch, after_delete, after_options
-{% for type in ["before", "after"] %}
-  {% for method in FILTER_METHODS %}
-    def {{type.id}}_{{method.id}}(path = "*", &block : HTTP::Server::Context -> _)
-     Kemal::FilterHandler::INSTANCE.{{type.id}}({{method}}.upcase, path, &block)
+    def ws(path, &block : HTTP::WebSocket, HTTP::Server::Context -> Void)
+      raise Kemal::Exceptions::InvalidPathStartException.new("ws", path) unless Kemal::Utils.path_starts_with_slash?(path)
+      Kemal::WebSocketHandler.new path, &block
     end
-  {% end %}
-{% end %}
+
+    def error(status_code, &block : HTTP::Server::Context, Exception -> _)
+      Kemal.config.add_error_handler status_code, &block
+    end
+
+    # All the helper methods available are:
+    #  - before_all, before_get, before_post, before_put, before_patch, before_delete, before_options
+    #  - after_all, after_get, after_post, after_put, after_patch, after_delete, after_options
+    {% for type in ["before", "after"] %}
+      {% for method in FILTER_METHODS %}
+        def {{type.id}}_{{method.id}}(path = "*", &block : HTTP::Server::Context -> _)
+         Kemal::FilterHandler::INSTANCE.{{type.id}}({{method}}.upcase, path, &block)
+        end
+      {% end %}
+    {% end %}
+  end
+
+  extend DSL
+end
+
+extend Kemal::DSL
