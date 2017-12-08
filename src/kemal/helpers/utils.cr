56d34deef97dfef9f4534ec58aeca54a8fa8694b
Serdar Dogruyol <dogruyolserdar@gmail.com>
2017-12-08 21:35:02 +0300

    Minor refactors

--- src/kemal/helpers/utils.cr
+++ src/kemal/helpers/utils.cr
@@ -1,11 +1,13 @@
 module Kemal
   module Utils
+    ZIP_TYPES = [".htm", ".html", ".txt", ".css", ".js", ".svg", ".json", ".xml", ".otf", ".ttf", ".woff", ".woff2"]
+
     def self.path_starts_with_slash?(path : String)
       path.starts_with?("/")
     end
 
     def self.zip_types(path : String) # https://github.com/h5bp/server-configs-nginx/blob/master/nginx.conf
-      [".htm", ".html", ".txt", ".css", ".js", ".svg", ".json", ".xml", ".otf", ".ttf", ".woff", ".woff2"].includes? File.extname(path)
+      ZIP_TYPES.includes? File.extname(path)
     end
 
     def self.mime_type(path : String)
