c3dd24482ec8379004ffd8aa61258995b80d0b41
Anton Maminov <anton.linux@gmail.com>
2018-12-25 18:06:07 +0200

    set status_code before response (#513)

--- src/kemal/exception_handler.cr
+++ src/kemal/exception_handler.cr
@@ -20,8 +20,8 @@ module Kemal
     private def call_exception_with_status_code(context : HTTP::Server::Context, exception : Exception, status_code : Int32)
       if !Kemal.config.error_handlers.empty? && Kemal.config.error_handlers.has_key?(status_code)
         context.response.content_type = "text/html" unless context.response.headers.has_key?("Content-Type")
-        context.response.print Kemal.config.error_handlers[status_code].call(context, exception)
         context.response.status_code = status_code
+        context.response.print Kemal.config.error_handlers[status_code].call(context, exception)
         context
       end
     end
