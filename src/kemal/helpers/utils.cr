b7bf9d771f27edda28d7285f5b25f879641f36fb
Sijawusz Pur Rahnama <sija@sija.pl>
2019-02-05 08:26:45 +0100

    Use Crystal built-in MIME registry (#516)

--- src/kemal/helpers/utils.cr
+++ src/kemal/helpers/utils.cr
@@ -9,27 +9,5 @@ module Kemal
     def self.zip_types(path : String) # https://github.com/h5bp/server-configs-nginx/blob/master/nginx.conf
       ZIP_TYPES.includes? File.extname(path)
     end
-
-    def self.mime_type(path : String)
-      case File.extname(path)
-      when ".txt"          then "text/plain"
-      when ".htm", ".html" then "text/html"
-      when ".css"          then "text/css"
-      when ".js"           then "application/javascript"
-      when ".png"          then "image/png"
-      when ".jpg", ".jpeg" then "image/jpeg"
-      when ".gif"          then "image/gif"
-      when ".svg"          then "image/svg+xml"
-      when ".ico"          then "image/x-icon"
-      when ".xml"          then "application/xml"
-      when ".json"         then "application/json"
-      when ".otf", ".ttf"  then "application/font-sfnt"
-      when ".woff"         then "application/font-woff"
-      when ".woff2"        then "font/woff2"
-      when ".mp4"          then "video/mp4"
-      when ".webm"         then "video/webm"
-      else                      "application/octet-stream"
-      end
-    end
   end
 end
