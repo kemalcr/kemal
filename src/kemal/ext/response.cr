497ef770f1087039a78a5e602cdd4d7aa9ed2c95
Anton Maminov <anton.linux@gmail.com>
2019-06-14 18:08:09 +0300

    fix issue with simple example

--- src/kemal/ext/response.cr
+++ src/kemal/ext/response.cr
@@ -1,7 +1,7 @@
 class HTTP::Server::Response
   class Output
     def close
-      if !response.wrote_headers? && response.headers.has_key?("Content-Range")
+      unless response.wrote_headers? && !response.headers.has_key?("Content-Range")
         response.content_length = @out_count
       end
 
