c7bcad3a6d73b76db3e373be1f016b72513d8bb2
Anton Maminov <anton.linux@gmail.com>
2020-06-22 21:02:48 +0300

    Update src/kemal/websocket_handler.cr
    Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

--- src/kemal/websocket_handler.cr
+++ src/kemal/websocket_handler.cr
@@ -11,7 +11,7 @@ module Kemal
 
     def call(context : HTTP::Server::Context)
       return call_next(context) unless context.ws_route_found? && websocket_upgrade_request?(context)
-      content = context.websocket.call(context)
+      context.websocket.call(context)
     end
 
     def lookup_ws_route(path : String)
