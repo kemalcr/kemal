926206a46c85bb1ade98a33cc063d7cc423154ba
Oleh Prypin <oleh@pryp.in>
2021-04-11 11:17:50 +0200

    Allow running specs from any directory (#609)

--- spec/view_spec.cr
+++ spec/view_spec.cr
@@ -1,14 +1,14 @@
 require "./spec_helper"
 
 macro render_with_base_and_layout(filename)
-  render "spec/asset/#{{{filename}}}", "spec/asset/layout.ecr"
+  render "#{__DIR__}/asset/#{{{filename}}}", "#{__DIR__}/asset/layout.ecr"
 end
 
 describe "Views" do
   it "renders file" do
     get "/view/:name" do |env|
       name = env.params.url["name"]
-      render "spec/asset/hello.ecr"
+      render "#{__DIR__}/asset/hello.ecr"
     end
     request = HTTP::Request.new("GET", "/view/world")
     client_response = call_request_on_app(request)
@@ -28,7 +28,7 @@ describe "Views" do
   it "renders layout" do
     get "/view/:name" do |env|
       name = env.params.url["name"]
-      render "spec/asset/hello.ecr", "spec/asset/layout.ecr"
+      render "#{__DIR__}/asset/hello.ecr", "#{__DIR__}/asset/layout.ecr"
     end
     request = HTTP::Request.new("GET", "/view/world")
     client_response = call_request_on_app(request)
@@ -40,7 +40,7 @@ describe "Views" do
       name = env.params.url["name"]
       var1 = "serdar"
       var2 = "kemal"
-      render "spec/asset/hello_with_content_for.ecr", "spec/asset/layout_with_yield_and_vars.ecr"
+      render "#{__DIR__}/asset/hello_with_content_for.ecr", "#{__DIR__}/asset/layout_with_yield_and_vars.ecr"
     end
     request = HTTP::Request.new("GET", "/view/world")
     client_response = call_request_on_app(request)
@@ -52,7 +52,7 @@ describe "Views" do
   it "renders layout with content_for" do
     get "/view/:name" do |env|
       name = env.params.url["name"]
-      render "spec/asset/hello_with_content_for.ecr", "spec/asset/layout_with_yield.ecr"
+      render "#{__DIR__}/asset/hello_with_content_for.ecr", "#{__DIR__}/asset/layout_with_yield.ecr"
     end
     request = HTTP::Request.new("GET", "/view/world")
     client_response = call_request_on_app(request)
