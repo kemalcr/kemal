328174baa72d7e818c3a749228cc91a400055204
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2021-11-27 14:27:36 -0600

    Move config code over to Application

--- src/kemal/application.cr
+++ src/kemal/application.cr
@@ -9,6 +9,15 @@ module Kemal
     getter(filter_handler) { FilterHandler.new(self) }
     getter(config) { Config.new(self) }
 
+    @handlers = [] of HTTP::Handler
+    @custom_handlers = [] of Tuple(Nil | Int32, HTTP::Handler)
+    @filter_handlers = [] of HTTP::Handler
+    @error_handlers = {} of Int32 => HTTP::Server::Context, Exception -> String
+    @error_handler : HTTP::Handler?
+    @router_included = false
+    @default_handlers_setup = false
+    @handler_position = 0
+
     {% for method in HTTP_METHODS %}
       def {{method.id}}(path : String, &block : HTTP::Server::Context -> _)
         raise Kemal::Exceptions::InvalidPathStartException.new({{method}}, path) unless Kemal::Utils.path_starts_with_slash?(path)
@@ -22,7 +31,7 @@ module Kemal
     end
 
     def error(status_code : Int32, &block : HTTP::Server::Context, Exception -> _)
-      Kemal.config.add_error_handler(status_code, &block)
+      @error_handlers[status_code] = ->(context : HTTP::Server::Context, error : Exception) { block.call(context, error).to_s }
     end
 
     # All the helper methods available are:
@@ -41,5 +50,95 @@ module Kemal
         end
       {% end %}
     {% end %}
+
+    def clear
+      config.powered_by_header = true
+      @router_included = false
+      @handler_position = 0
+      @default_handlers_setup = false
+      @handlers.clear
+      @custom_handlers.clear
+      @filter_handlers.clear
+      @error_handlers.clear
+    end
+
+    def handlers
+      @handlers
+    end
+
+    def handlers=(handlers : Array(HTTP::Handler))
+      clear
+      @handlers.replace(handlers)
+    end
+
+    def add_handler(handler : HTTP::Handler)
+      @custom_handlers << {nil, handler}
+    end
+
+    def add_handler(handler : HTTP::Handler, position : Int32)
+      @custom_handlers << {position, handler}
+    end
+
+    def add_filter_handler(handler : HTTP::Handler)
+      @filter_handlers << handler
+    end
+
+    def error_handlers
+      @error_handlers
+    end
+
+    def setup
+      unless @default_handlers_setup && @router_included
+        setup_init_handler
+        setup_log_handler
+        setup_error_handler
+        setup_static_file_handler
+        setup_custom_handlers
+        setup_filter_handlers
+        @default_handlers_setup = true
+        @router_included = true
+        @handlers.insert(@handlers.size, Kemal::GLOBAL_APPLICATION.websocket_handler)
+        @handlers.insert(@handlers.size, Kemal::GLOBAL_APPLICATION.route_handler)
+      end
+    end
+
+    private def setup_init_handler
+      @handlers.insert(@handler_position, Kemal::GLOBAL_APPLICATION.init_handler)
+      @handler_position += 1
+    end
+
+    private def setup_log_handler
+      @handlers.insert(@handler_position, config.logger)
+      @handler_position += 1
+    end
+
+    private def setup_error_handler
+      if config.always_rescue
+        @error_handler ||= Kemal::ExceptionHandler.new
+        @handlers.insert(@handler_position, @error_handler.not_nil!)
+        @handler_position += 1
+      end
+    end
+
+    private def setup_static_file_handler
+      if config.serve_static.is_a?(Hash)
+        @handlers.insert(@handler_position, Kemal::StaticFileHandler.new(config.public_folder))
+        @handler_position += 1
+      end
+    end
+
+    private def setup_custom_handlers
+      @custom_handlers.each do |ch0, ch1|
+        position = ch0
+        @handlers.insert (position || @handler_position), ch1
+        @handler_position += 1
+      end
+    end
+
+    private def setup_filter_handlers
+      @filter_handlers.each do |h|
+        @handlers.insert(@handler_position, h)
+      end
+    end
   end
 end
--- src/kemal/config.cr
+++ src/kemal/config.cr
@@ -12,11 +12,6 @@ module Kemal
   class Config
     include ServerConfig
 
-    @handlers = [] of HTTP::Handler
-    @custom_handlers = [] of Tuple(Nil | Int32, HTTP::Handler)
-    @filter_handlers = [] of HTTP::Handler
-    @error_handlers = {} of Int32 => HTTP::Server::Context, Exception -> String
-
     property public_folder = "./public"
     property logging = true
     property always_rescue = true
@@ -26,16 +21,16 @@ module Kemal
     property app_name = "Kemal"
 
     @logger : Kemal::BaseLogHandler?
-    @error_handler : HTTP::Handler?
-    @router_included = false
-    @default_handlers_setup = false
-    @handler_position = 0
 
     def initialize(@app : Kemal::Application)
     end
 
-    def logger
-      @logger.not_nil!
+    def logger : Kemal::BaseLogHandler
+      @logger ||= if logging
+                    Kemal::LogHandler.new
+                  else
+                    Kemal::NullLogHandler.new
+                  end
     end
 
     def logger=(@logger : Kemal::BaseLogHandler)
@@ -46,102 +41,35 @@ module Kemal
     end
 
     def clear
-      @powered_by_header = true
-      @router_included = false
-      @handler_position = 0
-      @default_handlers_setup = false
-      @handlers.clear
-      @custom_handlers.clear
-      @filter_handlers.clear
-      @error_handlers.clear
+      @app.clear
     end
 
     def handlers
-      @handlers
+      @app.handlers
     end
 
     def handlers=(handlers : Array(HTTP::Handler))
-      clear
-      @handlers.replace(handlers)
+      @app.handlers = handlers
     end
 
     def add_handler(handler : HTTP::Handler)
-      @custom_handlers << {nil, handler}
+      @app.add_handler(handler)
     end
 
     def add_handler(handler : HTTP::Handler, position : Int32)
-      @custom_handlers << {position, handler}
+      @app.add_handler(handler, position)
     end
 
     def add_filter_handler(handler : HTTP::Handler)
-      @filter_handlers << handler
+      @app.add_filter_handler(handler)
     end
 
     def error_handlers
-      @error_handlers
-    end
-
-    def add_error_handler(status_code : Int32, &handler : HTTP::Server::Context, Exception -> _)
-      @error_handlers[status_code] = ->(context : HTTP::Server::Context, error : Exception) { handler.call(context, error).to_s }
+      @app.error_handlers
     end
 
     def setup
-      unless @default_handlers_setup && @router_included
-        setup_init_handler
-        setup_log_handler
-        setup_error_handler
-        setup_static_file_handler
-        setup_custom_handlers
-        setup_filter_handlers
-        @default_handlers_setup = true
-        @router_included = true
-        @handlers.insert(@handlers.size, Kemal::GLOBAL_APPLICATION.websocket_handler)
-        @handlers.insert(@handlers.size, Kemal::GLOBAL_APPLICATION.route_handler)
-      end
-    end
-
-    private def setup_init_handler
-      @handlers.insert(@handler_position, Kemal::GLOBAL_APPLICATION.init_handler)
-      @handler_position += 1
-    end
-
-    private def setup_log_handler
-      @logger ||= if @logging
-                    Kemal::LogHandler.new
-                  else
-                    Kemal::NullLogHandler.new
-                  end
-      @handlers.insert(@handler_position, @logger.not_nil!)
-      @handler_position += 1
-    end
-
-    private def setup_error_handler
-      if @always_rescue
-        @error_handler ||= Kemal::ExceptionHandler.new
-        @handlers.insert(@handler_position, @error_handler.not_nil!)
-        @handler_position += 1
-      end
-    end
-
-    private def setup_static_file_handler
-      if @serve_static.is_a?(Hash)
-        @handlers.insert(@handler_position, Kemal::StaticFileHandler.new(@public_folder))
-        @handler_position += 1
-      end
-    end
-
-    private def setup_custom_handlers
-      @custom_handlers.each do |ch0, ch1|
-        position = ch0
-        @handlers.insert (position || @handler_position), ch1
-        @handler_position += 1
-      end
-    end
-
-    private def setup_filter_handlers
-      @filter_handlers.each do |h|
-        @handlers.insert(@handler_position, h)
-      end
+      @app.setup
     end
   end
 end
