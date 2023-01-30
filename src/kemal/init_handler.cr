0f78cb99b2f6584719b1661601cbef5e5e065d89
Anton Maminov <anton.linux@gmail.com>
2023-01-30 14:40:57 +0200

    bump ameba to 1.4.0 and fix warnings

--- src/kemal/init_handler.cr
+++ src/kemal/init_handler.cr
@@ -7,7 +7,7 @@ module Kemal
     INSTANCE = new
 
     def call(context : HTTP::Server::Context)
-      context.response.headers.add "X-Powered-By", "Kemal" if Kemal.config.powered_by_header
+      context.response.headers.add "X-Powered-By", "Kemal" if Kemal.config.powered_by_header?
       context.response.content_type = "text/html" unless context.response.headers.has_key?("Content-Type")
       call_next context
     end
