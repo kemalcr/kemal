ba7cd7e7011dadbf4bba7bc901e26b74f52b0ffc
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2023-10-11 18:18:48 +0400

    Move to Process.on_interupt
    I remember this change was implemented before, I see that it is reverted now and I want that change back because developers on Windows are complaining that it broke their builds.

--- src/kemal.cr
+++ src/kemal.cr
@@ -89,10 +89,10 @@ module Kemal
   end
 
   private def self.setup_trap_signal
-    Signal::INT.trap do
+    Process.on_interrupt {
       log "#{Kemal.config.app_name} is going to take a rest!" if Kemal.config.shutdown_message
       Kemal.stop
       exit
-    end
+    }
   end
 end
