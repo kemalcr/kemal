d4c842e82ddb4d22fc3690ef93bb3bcd22eff367
Serdar Dogruyol <990485+sdogruyol@users.noreply.github.com>
2025-04-03 17:54:17 +0300

    Remove depracated custom logger spec

--- spec/helpers_spec.cr
+++ spec/helpers_spec.cr
@@ -22,12 +22,6 @@ describe "Macros" do
       logging false
       Kemal.config.logging.should eq false
     end
-
-    it "sets a custom logger" do
-      config = Kemal::Config::INSTANCE
-      logger CustomLogHandler.new
-      config.logger.should be_a(CustomLogHandler)
-    end
   end
 
   describe "#halt" do
