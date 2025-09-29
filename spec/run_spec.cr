5bd65bcb737d38ee3ecec708ac0426a1020d93f3
Serdar Dogruyol <990485+sdogruyol@users.noreply.github.com>
2025-09-29 14:29:26 +0300

    improve run_spec, move Kemal configuration to run method and update server bind ports

--- spec/run_spec.cr
+++ spec/run_spec.cr
@@ -3,6 +3,10 @@ require "./spec_helper"
 private def run(code)
   code = <<-CR
     require "./src/kemal"
+
+    Kemal.config.env = "test"
+    Kemal.config.port = 8000
+
     #{code}
     CR
 
@@ -16,7 +20,6 @@ end
 describe "Run" do
   it "runs a code block after starting" do
     run(<<-CR).should contain("started")
-      Kemal.config.env = "test"
       Kemal.run do
         log "started"
       end
@@ -25,7 +28,6 @@ describe "Run" do
 
   it "runs a code block after stopping" do
     run(<<-CR).should contain("stopped")
-      Kemal.config.env = "test"
       Kemal.run do
         Kemal.stop
         log "stopped"
@@ -35,7 +37,6 @@ describe "Run" do
 
   it "runs without a block being specified" do
     run(<<-CR).should contain "[test] Kemal is running in test mode."
-      Kemal.config.env = "test"
       Kemal.run
       Kemal.config.running
       CR
@@ -43,15 +44,14 @@ describe "Run" do
 
   it "allows custom HTTP::Server bind" do
     run(<<-CR).should contain "[test] Kemal is running in test mode."
-      Kemal.config.env = "test"
       Kemal.run do |config|
         server = config.server.not_nil!
 
         {% if flag?(:windows) %}
-          server.bind_tcp "127.0.0.1", 3000
+          server.bind_tcp "127.0.0.1", 8000
         {% else %}
-          server.bind_tcp "127.0.0.1", 3000, reuse_port: true
-          server.bind_tcp "0.0.0.0", 3001, reuse_port: true
+          server.bind_tcp "127.0.0.1", 8000, reuse_port: true
+          server.bind_tcp "0.0.0.0", 8001, reuse_port: true
         {% end %}
       end
       CR
