b8eb7b19232d2cf84374fd4d2695029aabcd5965
Serdar Dogruyol <990485+sdogruyol@users.noreply.github.com>
2026-08-02 10:25:25 +0300

    Extract path prefix matching into Kemal::Utils. Share the same prefix rules between PathHandler and Handler only/exclude.

--- src/kemal/helpers/utils.cr
+++ src/kemal/helpers/utils.cr
@@ -9,5 +9,14 @@ module Kemal
     def self.zip_types(path : String) # https://github.com/h5bp/server-configs-nginx/blob/master/nginx.conf
       ZIP_TYPES.includes? File.extname(path)
     end
+
+    # Exact prefix, or prefix followed by `/`. `"/"`, and `""` match all paths.
+    def self.matches_path_prefix?(prefix : String, path : String) : Bool
+      return true if prefix.in?("/", "")
+
+      return true if path == prefix
+
+      path.starts_with?("#{prefix}/")
+    end
   end
 end
